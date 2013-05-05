28dab62cda927827df90801270c76dadc78f2ec4
Spanone <spanone.alex@gmail.com>
2013-05-04 22:29:24 -0700

    AlphaFirst

---
+++ mod/spanone/alphamod/alphaMod.java
@@ -0,0 +1,29 @@
+package mod.spanone.alphamod;
+
+import mod.spanone.alphamod.lib.Reference;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+
+@Mod( modid=Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+public class alphaMod {
+	
+	@PreInit
+	public void preInit(FMLPreInitializationEvent event){
+		
+	}
+	@Init
+	public void preInit(FMLInitializationEvent event){
+		
+	}
+	@PostInit
+	public void preInit(FMLPostInitializationEvent event){
+		
+	}
+		
+}
---
+++ mod/spanone/alphamod/lib/Reference.java
@@ -0,0 +1,9 @@
+package mod.spanone.alphamod.lib;
+
+public class Reference {
+	
+	public static final String MOD_ID = "AlphaSpanone";
+	public static final String MOD_NAME = "Alpha Mod";
+	public static final String VERSION = "1.5.1.1";
+
+}
